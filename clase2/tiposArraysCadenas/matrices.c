00bf1988ead3e3542556856b3b5e8049be04605d
carrodher <carrodher1179@gmail.com>
2016-03-30 00:13:35 +0200

    Ejercicios tipos y arrays

--- clase2/tiposArraysCadenas/matrices.c
+++ clase2/tiposArraysCadenas/matrices.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 
-int main() {
-    int matriz1[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int matriz2[3][3] = {{11,22,33},{44,55,66},{77,88,99}};
+#define TAM 3
 
+int main() {
+    int matriz1[TAM][TAM] = {{1,2,3},{4,5,6},{7,8,9}};
+    int matriz2[TAM][TAM] = {{11,22,33},{44,55,66},{77,88,99}};
+
+    /* Imprime ambas matrices normal */
     printf("Matriz 1:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\n",matriz1[i][j]);
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ",matriz1[i][j]);
         }
+        printf("\n");
     }
 
     printf("\nMatriz 2:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\n",matriz2[i][j]);
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ",matriz2[i][j]);
         }
+        printf("\n");
     }
 
+    /* Multiplica la matriz 1 por 2 */
     printf("\nMatriz 1 por 2:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\n",matriz1[i][j]*2);
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ",matriz1[i][j]*2);
         }
+        printf("\n");
     }
 
+    /* Multiplica la matriz 1 por la matriz 2*/
     printf("\nMatriz 1 por Matriz 2:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d\n",matriz1[i][j]*matriz2[i][j]);
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ",matriz1[i][j]*matriz2[i][j]);
         }
+        printf("\n");
     }
 
+    return 0;
 }
--- clase2/tiposArraysCadenas/printSize.c
+++ clase2/tiposArraysCadenas/printSize.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-/* Hemos hecho pruebas con diferentes tipos de datos, superando los límites
-para ver los warnings por overflow y ver qué valor devuelven
-*/
 int main() {
-    int entero = 3;
-    char c;
-    c = 'c';
+    int entero = 3;         // Variable entera
+    char c = 'c';           // Variable caracter
 
     printf("int\n\tvalor = %d\n\ttamaño %lu bytes \n\n",entero, sizeof(entero));
     printf("char\n\tvalor = %c\n\ttamaño %lu bytes \n\n",c, sizeof(char));
